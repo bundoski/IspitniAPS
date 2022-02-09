aebe6159069cb1f2f9a1db2a1ff2f47aae4d56d9
sadastro <93097556+sadastro@users.noreply.github.com>
2022-02-09 16:03:41 +0100

    Update pettaZadaca.java

--- listi/pettaZadaca.java
+++ listi/pettaZadaca.java
@@ -4,10 +4,18 @@ import java.io.InputStreamReader;
 
 /*
 5. Lista
-
-Na vlez se prima lista I taa lista treba da se modificira na nacin so pomegju sekoi 2 broja
- ke se vmetne broj sto e aritmeticka sredina na tie 2 broja. Da ne se koristi pomoshna lista
- tuku da se vmetnat vo vekje dadenata.
+// You are given a list (SLL, element type Integer) which needs to be modified in a way in which between every two elements you need to
+// insert the mean value of those two numbers.
+// You are forbidden to use additional lists, rather you have to insert the value in the given list.
+// input:
+//      first line: number of elements
+//      second line: elements
+// Example 1:
+//  input:
+//      8
+//      1 3 5 7 9 11 13 15
+//  output:
+//      1.0 2.0 3.0 4.0 5.0 6.0 7.0 8.0 9.0 10.0 11.0 12.0 13.0 14.0 15.0
 
  */
 

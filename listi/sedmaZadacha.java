f8e221c615df449a8e6d0c5f0d362d239de522ad
sadastro <93097556+sadastro@users.noreply.github.com>
2022-02-09 14:59:03 +0100

    Add files via upload

---
+++ listi/sedmaZadacha.java
@@ -0,0 +1,54 @@
+/*
+// You are given a single linked list filled with integers.
+// You have to implement a deletion system in which you keep the first element, remove the second,
+then you keep the next two elements, but delete
+// their successor and so one while the limitations of the list allow you to do so.
+// input:
+//      first line: number of elements
+//      second line: elements
+// Constraints: You are forbidden to use any additional structures like arrays and to change the value of the nodes.
+// Example 1:
+//  input:
+//      7
+//      1 6 9 5 6 3 0
+//  output:
+//      1 9 5 3 0
+ */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class sedmaZadacha {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int numberOfElements = Integer.parseInt(br.readLine());
+
+        SLL<Integer> integerSLL = new SLL<>();
+
+        String[] elements = br.readLine().split(" ");
+
+        for (int i = 0; i < numberOfElements; i++)
+            integerSLL.insertLast(Integer.parseInt(elements[i]));
+
+        int numberOfSteps = 1;
+        int numberOfStepsChangeable = 1;
+        SLLNode<Integer> node = integerSLL.getFirst();
+        while(node != null) {
+            if(numberOfStepsChangeable == 0) {
+                SLLNode<Integer> tempNode = node.succ;
+                integerSLL.delete(node);
+                numberOfStepsChangeable = ++numberOfSteps;
+                node = tempNode;
+            } else {
+                numberOfStepsChangeable--;
+                node = node.succ;
+            }
+        }
+
+        System.out.println(integerSLL);
+
+    }
+
+}
